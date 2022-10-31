092a361eb2439313d4f2b3ed46278e121040bcc2
Matias Hausen <matihausen@gmail.com>
2022-10-31 12:45:30 -0300

    Proyecto finalizado, falta javascript

---
+++ script.js
@@ -0,0 +1,16 @@
+//Agregar producto
+
+const imagenProduto = document.getElementById("urlFoto").value;
+
+const categoria = document.getElementById("categoria").value;
+
+const titulo = document.getElementById("tituloProd").value;
+
+const descripcion = document.getElementById("descripcion__producto").value;
+
+const precio = document.getElementById("precioPro").value;
+
+const receptor = document.getElementById("items__todos"); //Aca van productos
+
+
+document.crea
